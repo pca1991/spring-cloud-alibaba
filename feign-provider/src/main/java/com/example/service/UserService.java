e666a2b8665a596660ab680fea3879de6eb50e3a
qxu <qxu@easypay.com.cn>
2021-02-02 11:03:15 +0800

    feign nacos demo

---
+++ feign-provider/src/main/java/com/example/service/UserService.java
@@ -0,0 +1,19 @@
+package com.example.service;
+
+import com.example.domain.User;
+
+import java.util.List;
+
+public interface UserService {
+    void create(User user);
+
+    User getUser(Long id);
+
+    void update(User user);
+
+    void delete(Long id);
+
+    User getByUsername(String username);
+
+    List<User> getUserByIds(List<Long> ids);
+}
